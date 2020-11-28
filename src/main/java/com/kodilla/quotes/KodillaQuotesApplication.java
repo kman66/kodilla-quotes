0d5830c844871d6a182bcc8d850c544d454dd577
vls6 <mress@tutanota.com>
2020-11-28 15:41:22 +0100

    initial commit

---
+++ src/main/java/com/kodilla/quotes/KodillaQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/quotes/KodillaQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
